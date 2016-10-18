3402da76eff369835c776f8b0e0dfd356ac7aec8
Kylejwilliams <kjwkc3@mail.umsl.edu>
2016-10-18 13:12:01 -0500

    Implemented time handling

--- oss.c
+++ oss.c
@@ -3,26 +3,38 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+typedef struct { int sec, nsec; } tick_t; /* represents a clock containing the time in seconds and nanoseconds */
+const int NSECS_IN_SECS = 1000000000; /* 10^9 nanoseconds per second */
 
 int main(int argc, char *argv[])
 {
-    int         opt;                                                        /* holds the current command line argument */
-    const char  *short_opt = "hs:l:t:";                                     /* valid args */
-    struct      option long_opt[] = { { "help", no_argument, NULL, 'h' } }; /* valid non-char args */
-    int         num_slaves = 5;                                             /* max number of slave processes spawned */
-    char        *filename = "log.txt";                                      /* log file */
-    int         timeout = 20;                                               /* time before master should kill itself */
-    int         i;                                                          /* iterator */
+    int         opt;															/* holds the current command line argument */
+    const char  *short_opt			= "hs:l:t:";								/* valid args */
+    struct      option long_opt[]	= { { "help", no_argument, NULL, 'h' } };	/* valid non-char args */
+    int         num_slaves			= 5;                                        /* max number of slave processes spawned */
+    char        *filename			= "log.txt";                                /* log file */
+    int         timeout				= 20;                                       /* time before master should kill itself */
+    int         i;																/* iterator */
+	tick_t		clock				= { 0, 0 };									/* time elapsed since execution began */
 
     while ((opt = getopt_long(argc, argv, short_opt, long_opt, NULL)) != -1)
     {
         switch (opt)
         {
             case -1:
-            case 0:
-                break;
-            case 'h':
+				break;
+			case ':':
+			case '?':
+				abort();
+			case 'h':
                 printf("Usage: %s [OPTIONS]\n", argv[0]);
+				printf("\t-h, --help\tprints this help message and exits\n");
+				printf("\t-s x\t\tmaximum number of slave processes to spawn\n");
+				printf("\t-l filename\tname of the log file to output to\n");
+				printf("\t-t z\t\tamount of time (in seconds) before the master process should terminate\n");
                 return 0;
                 break;
             case 's':
@@ -36,6 +48,12 @@ int main(int argc, char *argv[])
                 break;
         }
     }
+	
+	/* initialize some shared memory vars */
+	key_t key_clock = ftok("/tmp", 44);
+	int shmid_clock = shmget(key_clock, sizeof(tick_t), IPC_CREAT | 0666);
+	tick_t *shm_clock = shmat(shmid_clock, NULL, 0);
+	*shm_clock = clock;
 
     /* spawn some slaves */
     pid_t   slaves[num_slaves]; /* array of slaves by pid */
@@ -52,6 +70,17 @@ int main(int argc, char *argv[])
         else if (slaves[i] == 0)
             execl("./user", "user", NULL);
     }
+	
+	while (((*shm_clock).sec < timeout) && num_slaves > 0)
+	{
+		(*shm_clock).nsec++;
+		if ((*shm_clock).nsec % NSECS_IN_SECS == 0)
+		{
+			(*shm_clock).sec++;
+			(*shm_clock).nsec = 0;
+			printf("The current elapsed time is %ds:%dns\n", (*shm_clock).sec, (*shm_clock).nsec);
+		}
+	}
 
     while (num_slaves > 0)
     {
@@ -59,5 +88,12 @@ int main(int argc, char *argv[])
         printf("Child with PID %d has died\n", slave);
         --num_slaves;
     }
-    return (0);
+
+
+	/* cleanup all of our shared memory */
+	shmdt(shm_clock);
+	shmctl(shmid_clock, IPC_RMID, NULL);
+
+	/* program exited successfully */
+    exit(0);
 }
--- user.c
+++ user.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <getopt.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+typedef struct { int sec, nsec; } tick_t;
 
 int main(int argc, char *argv[])
 {
-    printf("I am process number %d and I have been spawned!\n", getpid());
+	/* get all of the shared memory variables */
+	key_t key_clock = ftok("/tmp", 44);
+	int shmid_clock = shmget(key_clock, sizeof(int), IPC_CREAT | 0666);
+	tick_t *shm_clock = shmat(shmid_clock, NULL, 0);
+	
+	printf("I am child number %d and the timeout variable has a value of %ds:%dns\n", getpid(), (*shm_clock).sec, (*shm_clock).nsec);
+
+	/* program exited successfully */
+	exit(0);
 }
