940fce166139c3c58b2b453f343b136fd6bb3378
Kylejwilliams <kjwkc3@mail.umsl.edu>
2016-10-20 02:37:57 -0500

    implemented messaging between processes

--- oss.c
+++ oss.c
@@ -5,11 +5,19 @@
 #include <wait.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/msg.h>
+#include <errno.h>
+#include <sys/sysinfo.h>
 
-typedef struct { int sec, nsec; } tick_t;   /* represents a clock containing the time in seconds and nanoseconds */
-const int NSECS_IN_SECS = 1000000000;       /* 10^9 nanoseconds per second */
-volatile sig_atomic_t term_flag = 0;        /* set this flag once the program has reached its timeout value*/
-volatile sig_atomic_t int_flag = 0;         /* set this flag when ctrl+c (interrupt) is thrown */
+typedef struct { int sec, nsec; } tick_t;                               /* represents a clock containing the time in seconds and nanoseconds */
+typedef struct { long mtype; int sec; int nsec; int pid; } msgbuf_t;    /* represents a message to be sent / received */
+typedef struct { long mtype; } prcmsgbuf_t;
+
+const int       NSECS_IN_SECS           = 1000000000;   /* 10^9 nanoseconds per second */
+const int       MAX_SLAVES              = 100;          /* max number of slaves to spawn before terminating the program */
+volatile        sig_atomic_t term_flag  = 0;            /* set this flag once the program has reached its timeout value */
+volatile        sig_atomic_t int_flag   = 0;            /* set this flag when ctrl+c (interrupt) is thrown */
+struct sysinfo  info;
 
 static void sig_handler(int signo)
 {
@@ -33,10 +41,11 @@ int main(int argc, char *argv[])
     const char  *short_opt			= "hs:l:t:";								/* valid args */
     struct      option long_opt[]	= { { "help", no_argument, NULL, 'h' } };	/* valid non-char args */
     int         num_slaves			= 5;                                        /* max number of slave processes spawned */
-    char        *filename			= "log.txt";                                /* log file */
+    //char        *filename			= (char *) "log.txt";                       /* log file */
     int         timeout				= 20;                                       /* time before master should kill itself */
     int         i;																/* iterator */
-	tick_t		clock				= { 0, 0 };									/* time elapsed since execution began */
+	//tick_t		tick				= { 0, 0 };									/* time elapsed since execution began */
+    pid_t       slaves[MAX_SLAVES];                                             /* array of slaves by pid */
 
     while ((opt = getopt_long(argc, argv, short_opt, long_opt, NULL)) != -1)
     {
@@ -59,7 +68,7 @@ int main(int argc, char *argv[])
                 num_slaves = atoi(optarg);
                 break;
             case 'l':
-                filename = optarg;
+                //filename = optarg;
                 break;
             case 't':
                 timeout = atoi(optarg);
@@ -67,21 +76,28 @@ int main(int argc, char *argv[])
         }
     }
 
-    /* initialize our alarm signal handler */
-    alarm(timeout);
-    signal(SIGALRM, sig_handler);
+
 
 	/* initialize some shared memory vars */
 	int key_clock = ftok("/tmp", 44);
-	int shmid_clock = shmget(key_clock, sizeof(tick_t), IPC_CREAT | 0666);
+	int shmid_clock = shmget(key_clock, sizeof(tick_t), 0666 | IPC_CREAT);
 	tick_t *shm_clock = (tick_t *)shmat(shmid_clock, NULL, 0);
-	*shm_clock = clock;
+	(*shm_clock).nsec = 0;
+    (*shm_clock).sec = 0;
+
+    key_t key_msg = ftok("/tmp", 97);
+    msgbuf_t msgbuf;
+    int msqid = msgget(key_msg, IPC_CREAT | 0666);
+    msgbuf.mtype = 0;
+    msgbuf.nsec = 0;
+    msgbuf.sec = 0;
+    msgbuf.pid = getpid();
+
+    key_msg = ftok("/tmp", 11);
+    prcmsgbuf_t pmb;
+    int pmbqid = msgget(key_msg, IPC_CREAT | 0666);
 
     /* spawn some slaves */
-    pid_t   slaves[num_slaves]; /* array of slaves by pid */
-    int     status;             /* returned message from spawned process */
-    pid_t   slave;              /* represents a single slave process */
-
     for (i = 0; i < num_slaves; i++)
     {
         if ((slaves[i] = fork()) < 0)
@@ -93,7 +109,16 @@ int main(int argc, char *argv[])
             execl("./user", "user", NULL);
     }
 
-	while (((*shm_clock).sec < 2))
+    /* initialize our alarm signal handler */
+    alarm(timeout);
+    signal(SIGALRM, sig_handler);
+
+    /* send the first message saying it's free */
+    pmb.mtype = 1;
+    if (msgsnd(pmbqid, &pmb, sizeof(pmb), 0) != 0)
+        perror("msgsnd");
+
+	while ((*shm_clock).sec < 2)
 	{
         /* interrupt was thrown; we need to exit */
         if (term_flag)
@@ -102,6 +127,7 @@ int main(int argc, char *argv[])
 
             shmdt(shm_clock);
             shmctl(shmid_clock, IPC_RMID, NULL);
+            msgctl(msqid, IPC_RMID, NULL);
 
             for (i = 0; i < num_slaves; i++)
                 kill(slaves[i], SIGTERM);
@@ -114,35 +140,48 @@ int main(int argc, char *argv[])
 
             shmdt(shm_clock);
             shmctl(shmid_clock, IPC_RMID, NULL);
+            msgctl(msqid, IPC_RMID, NULL);
 
             for (i = 0; i < num_slaves; i++)
                 kill(slaves[i], SIGTERM);
 
             exit(EXIT_SUCCESS);
         }
-        else
-        {
-            (*shm_clock).nsec++;
-    		if ((*shm_clock).nsec % NSECS_IN_SECS == 0)
-    		{
-    			(*shm_clock).sec++;
-    			(*shm_clock).nsec = 0;
-    			printf("The current elapsed time is %ds:%dns\n", (*shm_clock).sec, (*shm_clock).nsec);
-    		}
-        }
+
+        (*shm_clock).nsec++;
+		if ((*shm_clock).nsec % NSECS_IN_SECS == 0)
+		{
+			(*shm_clock).sec++;
+			(*shm_clock).nsec = 0;
+            printf("The current elapsed time is %ds:%dns\n", (*shm_clock).sec, (*shm_clock).nsec);
+		}
+
+        if (msgrcv(msqid, &msgbuf, sizeof(msgbuf), 2, IPC_NOWAIT) != -1)
+            printf("Master: Child %d is terminating at my time %d:%d because it reached %d:%d\n", msgbuf.pid, (*shm_clock).sec, (*shm_clock).nsec, msgbuf.sec, msgbuf.nsec);
+        // if (msgrcv(msqid, &msgbuf, sizeof(msgbuf), msgbuf.mtype, IPC_NOWAIT) == -1)
+        // {
+        //     if (errno == EINTR || errno == ENOMSG) break; /* don't worry if we're interrupted; being handled elsewhere */
+        //
+        //     perror("msgrcv");
+        //     exit(EXIT_FAILURE);
+        // }
+        // else
+        // {
+        //     printf("Master: Child %d is terminating at my time %d:%d because it reached %d:%d\n", msgbuf.pid, (*shm_clock).sec, (*shm_clock).nsec, msgbuf.sec, msgbuf.nsec);
+            /* spawn another slave */
+            //num_slaves++;
+
+            // if ((slaves[num_slaves - 1] = fork()) < 0)
+            //     num_slaves--;
+            // else if (slaves[num_slaves - 1] == 0)
+            //     execl("./user", "user", NULL);
+        //}
 	}
-
-    while (num_slaves > 0)
-    {
-        slave = wait(&status);
-        printf("Child with PID %d has died\n", slave);
-        --num_slaves;
-    }
-
 
 	/* cleanup all of our shared memory */
 	shmdt(shm_clock);
 	shmctl(shmid_clock, IPC_RMID, NULL);
+    msgctl(msqid, IPC_RMID, NULL);
 
 	/* program exited successfully */
     exit(EXIT_SUCCESS);
--- user.c
+++ user.c
@@ -5,18 +5,73 @@
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/msg.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
 
-typedef struct { int sec, nsec; } tick_t;
+typedef struct { int sec, nsec; } tick_t;                               /* represents a clock containing the time in seconds and nanoseconds */
+typedef struct { long mtype; int sec; int nsec; int pid; } msgbuf_t;    /* represents a message to be sent / received */
+typedef struct { long mtype; } prcmsgbuf_t;
 
 int main(int argc, char *argv[])
 {
+    int sleep_time = 0;
+    int start_time = 0;
+    int end_time = 0;
 	/* get all of the shared memory variables */
 	key_t key_clock = ftok("/tmp", 44);
 	int shmid_clock = shmget(key_clock, sizeof(int), IPC_CREAT | 0666);
-	tick_t *shm_clock = shmat(shmid_clock, NULL, 0);
-	
-	printf("I am child number %d and the timeout variable has a value of %ds:%dns\n", getpid(), (*shm_clock).sec, (*shm_clock).nsec);
+	tick_t *shm_clock = (tick_t *)shmat(shmid_clock, NULL, 0);
 
-	/* program exited successfully */
-	exit(0);
+    key_t   key_msg = ftok("/tmp", 97);
+    msgbuf_t msg;
+    int     msqid = msgget(key_msg, 0666);
+
+    key_msg = ftok("/tmp", 11);
+    prcmsgbuf_t pmb;
+    int pmbqid = msgget(key_msg, IPC_CREAT | 0666);
+
+    msgrcv(pmbqid, &pmb, sizeof(pmb), 1, 0);
+
+    // <<< critical section >>>
+
+    /* let other processes know cs is occupied */
+    //msgsnd(pmbqid, &pmb, sizeof(pmb), 2);
+
+    /* wait for a bit of time  */
+    sleep_time = rand() % 10;
+    start_time = ((*shm_clock).sec * 10) + (*shm_clock).nsec;
+    end_time = start_time + sleep_time;
+    while ((((*shm_clock).sec * 10) + (*shm_clock).nsec) < end_time) {  }
+
+    /* send our message to oss */
+    msg.nsec = (*shm_clock).nsec;
+    msg.sec = (*shm_clock).sec;
+    msg.pid = getpid();
+    msg.mtype = 2;
+    if (msgsnd(msqid, &msg, sizeof(msg), 0) != 0)
+        perror("msgsnd");
+
+    // <<< end critical section >>>
+
+    /* let the other processes know we're finished */
+    pmb.mtype = 1;
+    if (msgsnd(pmbqid, &pmb, sizeof(pmb), 0) != 0)
+        perror("msgsnd");
+
+    exit(EXIT_SUCCESS);
 }
+
+//void P(int i)
+//{
+//  message msg;
+//
+//  while (true)
+//  {
+//      receive(box, msg);
+//      /* critical section */
+//      send(box, msg);
+//      /* remainder */
+//  }
+//}
