52feb3a4b22544e15163221a35ed7dd2308e8d36
Kylejwilliams <kjwkc3@mail.umsl.edu>
2016-10-21 22:57:21 -0500

    complete

--- oss.c
+++ oss.c
@@ -7,7 +7,6 @@
 #include <sys/shm.h>
 #include <sys/msg.h>
 #include <errno.h>
-#include <sys/sysinfo.h>
 
 typedef struct { int sec, nsec; } tick_t;                               /* represents a clock containing the time in seconds and nanoseconds */
 typedef struct { long mtype; int sec; int nsec; int pid; } msgbuf_t;    /* represents a message to be sent / received */
@@ -17,7 +16,6 @@ const int       NSECS_IN_SECS           = 1000000000;   /* 10^9 nanoseconds per 
 const int       MAX_SLAVES              = 100;          /* max number of slaves to spawn before terminating the program */
 volatile        sig_atomic_t term_flag  = 0;            /* set this flag once the program has reached its timeout value */
 volatile        sig_atomic_t int_flag   = 0;            /* set this flag when ctrl+c (interrupt) is thrown */
-struct sysinfo  info;
 
 static void sig_handler(int signo)
 {
@@ -42,36 +40,36 @@ int main(int argc, char *argv[])
     const char  *short_opt			= "hs:l:t:";								/* valid args */
     struct      option long_opt[]	= { { "help", no_argument, NULL, 'h' } };	/* valid non-char args */
     int         num_slaves			= 5;                                        /* max number of slave processes spawned */
-    //char        *filename			= (char *) "log.txt";                       /* log file */
+    char        *filename			= (char *) "log.txt";                       /* log file */
     int         timeout				= 20;                                       /* time before master should kill itself */
     int         i;																/* iterator */
-	//tick_t		tick				= { 0, 0 };									/* time elapsed since execution began */
     pid_t       slaves[15];                                             /* array of slaves by pid */
-    for (i = 0; i < 15; i++) slaves[i] = -1;
+    for (i = 0; i < 15; i++) slaves[i] = 0;
+    FILE *fp;
 
     while ((opt = getopt_long(argc, argv, short_opt, long_opt, NULL)) != -1)
     {
         switch (opt)
         {
             case -1:
-				break;
-			case ':':
-			case '?':
-				abort();
-			case 'h':
+    				    break;
+      			case ':':
+      			case '?':
+     				    abort();
+      			case 'h':
                 printf("Usage: %s [OPTIONS]\n", argv[0]);
-				printf("\t-h, --help\tprints this help message and exits\n");
-				printf("\t-s x\t\tmaximum number of slave processes to spawn\n");
-				printf("\t-l filename\tname of the log file to output to\n");
-				printf("\t-t z\t\tamount of time (in seconds) before the master process should terminate\n");
-                return 0;
+      				  printf("\t-h, --help\tprints this help message and exits\n");
+      				  printf("\t-s x\t\tmaximum number of slave processes to spawn\n");
+      				  printf("\t-l filename\tname of the log file to output to\n");
+      				  printf("\t-t z\t\tamount of time (in seconds) before the master process should terminate\n");
+                exit(EXIT_SUCCESS);
                 break;
             case 's':
                 fprintf(stderr, "maximum allowed number of slaves is 15. setting num_slaves to 15\n");
                 num_slaves = (atoi(optarg) > 15) ? 15 : atoi(optarg);
                 break;
             case 'l':
-                //filename = optarg;
+                filename = optarg;
                 break;
             case 't':
                 timeout = atoi(optarg);
@@ -79,13 +77,16 @@ int main(int argc, char *argv[])
         }
     }
 
+    /* clear out any old content of the file */
+    fp = fopen(filename, "w");
+    fclose(fp);
 
 
-	/* initialize some shared memory vars */
-	int key_clock = ftok("/tmp", 44);
-	int shmid_clock = shmget(key_clock, sizeof(tick_t), 0666 | IPC_CREAT);
-	tick_t *shm_clock = (tick_t *)shmat(shmid_clock, NULL, 0);
-	(*shm_clock).nsec = 0;
+  	/* initialize some shared memory vars */
+  	int key_clock = ftok("/tmp", 44);
+  	int shmid_clock = shmget(key_clock, sizeof(tick_t), 0666 | IPC_CREAT);
+  	tick_t *shm_clock = (tick_t *)shmat(shmid_clock, NULL, 0);
+  	(*shm_clock).nsec = 0;
     (*shm_clock).sec = 0;
 
     key_t key_msg = ftok("/tmp", 97);
@@ -106,7 +107,11 @@ int main(int argc, char *argv[])
         if ((slaves[i] = fork()) < 0)
         {
             perror("fork error");
-            abort();
+            shmdt(shm_clock);
+            shmctl(shmid_clock, IPC_RMID, NULL);
+            msgctl(msqid, IPC_RMID, NULL);
+            msgctl(pmbqid, IPC_RMID, NULL);
+            exit(EXIT_FAILURE);
         }
         else if (slaves[i] == 0)
             execl("./user", "user", NULL);
@@ -116,13 +121,19 @@ int main(int argc, char *argv[])
     alarm(timeout);
     signal(SIGALRM, sig_handler);
 
-    /* send the first message saying it's free */
+    /* send the first message saying we're ready for messages */
     pmb.mtype = 1;
     if (msgsnd(pmbqid, &pmb, sizeof(pmb), 0) != 0)
+    {
         perror("msgsnd");
+        shmdt(shm_clock);
+        shmctl(shmid_clock, IPC_RMID, NULL);
+        msgctl(msqid, IPC_RMID, NULL);
+        msgctl(pmbqid, IPC_RMID, NULL);
+	  }
 
-	while ((*shm_clock).sec < 2 && num_slaves < 100)
-	{
+	  while ((*shm_clock).sec < 2 && num_slaves <= 100)
+	  {
         /* interrupt was thrown; we need to exit */
         if (term_flag)
         {
@@ -131,9 +142,7 @@ int main(int argc, char *argv[])
             shmdt(shm_clock);
             shmctl(shmid_clock, IPC_RMID, NULL);
             msgctl(msqid, IPC_RMID, NULL);
-
-            for (i = 0; i < num_slaves; i++)
-                kill(slaves[i], SIGTERM);
+            msgctl(pmbqid, IPC_RMID, NULL);
 
             exit(EXIT_SUCCESS);
         }
@@ -144,59 +153,70 @@ int main(int argc, char *argv[])
             shmdt(shm_clock);
             shmctl(shmid_clock, IPC_RMID, NULL);
             msgctl(msqid, IPC_RMID, NULL);
-
-            for (i = 0; i < num_slaves; i++)
-                kill(slaves[i], SIGTERM);
+            msgctl(pmbqid, IPC_RMID, NULL);
 
             exit(EXIT_SUCCESS);
         }
-
+		
+		    /* increment our process clock */
         (*shm_clock).nsec++;
-		if ((*shm_clock).nsec % NSECS_IN_SECS == 0)
-		{
-			(*shm_clock).sec++;
-			(*shm_clock).nsec = 0;
-            fprintf(stderr, "The current elapsed time is %ds:%dns\n", (*shm_clock).sec, (*shm_clock).nsec);
-		}
+		    if ((*shm_clock).nsec % NSECS_IN_SECS == 0)
+		    {
+		        (*shm_clock).sec++;
+		        (*shm_clock).nsec = 0;
+            //fprintf(stderr, "The current elapsed time is %ds:%dns\n", (*shm_clock).sec, (*shm_clock).nsec);
+		    }
 
         if (msgrcv(msqid, &msgbuf, sizeof(msgbuf), 2, IPC_NOWAIT) != -1)
         {
-            printf("Master: Child %d is terminating at my time %d:%d because it reached %d:%d\n", msgbuf.pid, (*shm_clock).sec, (*shm_clock).nsec, msgbuf.sec, msgbuf.nsec);
-            
+            /* write our received message to file */
+            fp = fopen(filename, "a");
+            fprintf(fp, "Master: Child %d is terminating at my time %d:%d because it reached %d:%d\n", msgbuf.pid, (*shm_clock).sec, (*shm_clock).nsec, msgbuf.sec, msgbuf.nsec);
+            fclose(fp);
+
             /* spawn another slave */
             num_slaves++;
-            int escape = 0;
+            int escape = 0; /* used to signal to break out of the loop */
             for (i = 0; i < 15; i++)
             {
-                if (escape == 1) break;
-                else if (kill(slaves[i], 0) == -1 && errno == ESRCH)
+                if (escape) break;
+                else if (slaves[i] == msgbuf.pid)
                 {
                     escape = 1;
+                    kill(slaves[i], SIGINT);
+
                     if ((slaves[i] = fork()) < 0)
                     {
                         perror("fork error");
                         shmdt(shm_clock);
                     	shmctl(shmid_clock, IPC_RMID, NULL);
                         msgctl(msqid, IPC_RMID, NULL);
-                        exit(EXIT_FAILURE);
-                    }
+                        msgctl(pmbqid, IPC_RMID, NULL);
+					          }
                     else if (slaves[i] == 0)
                         execl("./user", "user", NULL);
                 }
             }
-
-            /* let the other processes know we're finished */
-            pmb.mtype = 1;
-            if (msgsnd(pmbqid, &pmb, sizeof(pmb), 0) != 0)
-                perror("msgsnd");
         }
-	}
+    }
+	  fprintf(stderr, "Made it to the end successfully\n");
 
-	/* cleanup all of our shared memory */
-	shmdt(shm_clock);
-	shmctl(shmid_clock, IPC_RMID, NULL);
+	  int status;	
+	  for (i = 0; i < 15; i++)
+	  {
+		    if (kill(slaves[i], 0) == 0) /* process is still alive */
+		    {
+			    kill(slaves[i], SIGINT);
+			    //wait(&status); THIS SHOULD BE HERE BUT IT CAUSES THE PROGRAM TO HANG
+		    }
+	  }
+
+	  /* cleanup all of our shared memory */
+	  shmdt(shm_clock);
+	  shmctl(shmid_clock, IPC_RMID, NULL);
     msgctl(msqid, IPC_RMID, NULL);
+    msgctl(pmbqid, IPC_RMID, NULL);
 
-	/* program exited successfully */
+	  /* program exited successfully */
     exit(EXIT_SUCCESS);
 }
--- user.c
+++ user.c
@@ -9,20 +9,26 @@
 #include <errno.h>
 #include <string.h>
 #include <time.h>
+#include <wait.h>
 
 typedef struct { int sec, nsec; } tick_t;                               /* represents a clock containing the time in seconds and nanoseconds */
 typedef struct { long mtype; int sec; int nsec; int pid; } msgbuf_t;    /* represents a message to be sent / received */
 typedef struct { long mtype; } prcmsgbuf_t;
 
+volatile sig_atomic_t stop;
+void end(int sig) { stop = 1; }
+
 int main(int argc, char *argv[])
 {
+    /* variables to handle waiting inside the critical section */
     int sleep_time = 0;
     int start_time = 0;
     int end_time = 0;
-	/* get all of the shared memory variables */
-	key_t key_clock = ftok("/tmp", 44);
-	int shmid_clock = shmget(key_clock, sizeof(int), IPC_CREAT | 0666);
-	tick_t *shm_clock = (tick_t *)shmat(shmid_clock, NULL, 0);
+
+    /* get all of the shared memory variables */
+    key_t key_clock = ftok("/tmp", 44);
+    int shmid_clock = shmget(key_clock, sizeof(int), 0666);
+    tick_t *shm_clock = (tick_t *)shmat(shmid_clock, NULL, 0);
 
     key_t   key_msg = ftok("/tmp", 97);
     msgbuf_t msg;
@@ -30,45 +36,31 @@ int main(int argc, char *argv[])
 
     key_msg = ftok("/tmp", 11);
     prcmsgbuf_t pmb;
-    int pmbqid = msgget(key_msg, IPC_CREAT | 0666);
-
+    int pmbqid = msgget(key_msg, 0666);
+    
+    /* wait until it's our turn to enter the critical section */
     msgrcv(pmbqid, &pmb, sizeof(pmb), 1, 0);
+    
+    // <<< begin critical section >>>
+			
+		/* wait for a bit of time  while inside the critical section */
+		sleep_time = rand() % 100000;
+		start_time = ((*shm_clock).sec * 1000000000) + (*shm_clock).nsec;
+		end_time = start_time + sleep_time;
+		while ((((*shm_clock).sec * 1000000000) + (*shm_clock).nsec) < end_time) {  }
 
-    // <<< critical section >>>
+		/* send our message to oss */
+		msg.nsec = (*shm_clock).nsec;
+		msg.sec = (*shm_clock).sec;
+		msg.pid = getpid();
+		msg.mtype = 2;
+		msgsnd(msqid, &msg, sizeof(msg), 0);
 
-    /* let other processes know cs is occupied */
-    //msgsnd(pmbqid, &pmb, sizeof(pmb), 2);
-
-    /* wait for a bit of time  */
-    sleep_time = rand() % 10;
-    start_time = ((*shm_clock).sec * 10) + (*shm_clock).nsec;
-    end_time = start_time + sleep_time;
-    while ((((*shm_clock).sec * 10) + (*shm_clock).nsec) < end_time) {  }
-
-    /* send our message to oss */
-    msg.nsec = (*shm_clock).nsec;
-    msg.sec = (*shm_clock).sec;
-    msg.pid = getpid();
-    msg.mtype = 2;
-    if (msgsnd(msqid, &msg, sizeof(msg), 0) != 0)
-        perror("msgsnd");
-
-    // <<< end critical section >>>
-
-
-
-    exit(EXIT_SUCCESS);
+		// <<< end critical section >>>
+	
+    /* let the other processes know we're finished and terminate */
+	  pmb.mtype = 1;
+	  msgsnd(pmbqid, &pmb, sizeof(pmb), 0);
+			
+	  exit(EXIT_SUCCESS);
 }
-
-//void P(int i)
-//{
-//  message msg;
-//
-//  while (true)
-//  {
-//      receive(box, msg);
-//      /* critical section */
-//      send(box, msg);
-//      /* remainder */
-//  }
-//}
